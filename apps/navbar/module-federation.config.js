cc4e1947be4748301e93a4b9d0670dc2cafe4607
Dominik Blau <dominik.blau@gmail.com>
2022-05-28 17:09:57 +0200

    initial mfe commit

---
+++ apps/navbar/module-federation.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'navbar',
+  exposes: {
+    './Module': 'apps/navbar/src/app/remote-entry/entry.module.ts',
+  },
+};
---
+++ apps/navbar/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'navbar-entry',
+  template: `<div> NAVBAR REMOTE</div>`,
+})
+export class RemoteEntryComponent {}
